1ca46b817828eda078b557acb1dfef8d3d58a4f4
Code Concisely <codeconcisely@gmail.com>
2021-02-22 16:02:27 +0200

    Add auto shutdown script

---
+++ auto_shutdown.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+SERVICE="spigot.jar"
+LOGS=/home/ec2-user/spigot_server/logs/latest.log
+NO_PLAYERS="There are 0 of a max of 20 players online"
+VERBOSE_MODE=0
+
+while getopts v flag
+do
+    case "${flag}" in
+        v) VERBOSE_MODE=1;;
+    esac
+done
+
+function get_players() {
+    sudo tmux send-keys -t "minecraft_session" "list" Enter
+    sleep 5s
+    PLAYERS=$(tail -n 1 $LOGS | cut -d ":" -f 4 | xargs)
+}
+
+function shutdown() {
+    sudo shutdown -h now
+}
+
+function log() {
+    if [[ "$VERBOSE_MODE" = 1 ]]; then
+        echo "$1"
+    fi
+}
+
+if ps aux | grep $SERVICE | grep -v grep > /dev/null
+then
+    log "$SERVICE is running"
+else
+    log "$SERVICE is stopped"
+    shutdown
+fi
+
+get_players
+log $PLAYERS
+if [[ "$PLAYERS" == "$NO_PLAYERS" ]]
+then
+    log "EMPTY"
+    sleep 5m
+    get_players
+    if [[ "$PLAYERS" == "$NO_PLAYERS" ]]
+    then
+        log "STILL EMPTY - SHUTDOWN"
+	shutdown
+    fi
+else
+    log "NOT EMPTY"
+fi
