0c30a6bcb412bdf36e55e7221ebd7985b0abdf99
Code Concisely <codeconcisely@gmail.com>
2021-02-23 13:35:47 +0200

    Update install_tools to accept directory argument

--- install_tools.sh
+++ install_tools.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-DIR="/opt/minecraft/BuildTools"
+DIR="."
 REV="latest"
 
-while getopts "hr:" OPTION; do
+while getopts "hd:v:" OPTION; do
   case "$OPTION" in
+    d)
+      DIR="$OPTARG";;
     h)
-      echo -e "Installs latest server version by default.\n"
-      echo "  -r version      Install a specific server version"
-      echo "                  Example: -r 1.16.5"
-      ;;
+      echo -e "Usage: ./install_tools.sh [-d path] [-v version]"
+      echo -e "Example: ./install_tools.sh -d /opt/minecraft/BuildTools -v 1.16.5 \n"
+      echo -e "Installs the latest server version in current directory by default.\n"
+      echo "  -d path         Specify a path where to install server files"
+      echo "  -v version      Install a specific server version"
+      exit 0;;
 
     r)
       REV="$OPTARG";;
