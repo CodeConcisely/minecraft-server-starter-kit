e4a32b029667e912e353091c814524567738d2a5
Code Concisely <codeconcisely@gmail.com>
2021-02-24 10:48:14 +0200

    Add cleanup script

---
+++ cleanup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+FORCE_MODE=0
+
+while getopts "f" OPTION; do
+  case "$OPTION" in
+      f) FORCE_MODE=1;;
+      ?) exit 1;
+  esac
+done
+
+function cleanup() {
+    sudo userdel minecraft
+    sudo groupdel minecraft
+    sudo rm -rf /opt/minecraft/
+    echo "--- Cleaned up ---"
+}
+
+echo "--- Starting cleanup ---"
+
+if [[ $FORCE_MODE = 1 ]]
+then
+  cleanup
+else
+  read -p "Are you sure you want to delete minecraft user and group and /opt/minecraft? [y/N] " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    cleanup
+  else
+    echo "--- Cleanup canceled ---"
+  fi
+fi
+
+
