097b825e11d800a9b6cc57a3780da8a62ea3196c
Code Concisely <codeconcisely@gmail.com>
2021-02-25 14:43:59 +0200

    Fix setup script to use correct folder

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 SERVER_DIR="/opt/minecraft/server"
 TOOLS_DIR="/opt/minecraft/BuildTools"
 SERVER_VERSION="latest"
+SCRIPTS_DIR=$(pwd)
 
 SETUP_STEPS=5
 
@@ -17,7 +18,7 @@ echo "--- Running tools installation 3/$SETUP_STEPS ---"
 . install_tools.sh -d $TOOLS_DIR -v $SERVER_VERSION
 
 echo "--- Setting up server 4/$SETUP_STEPS ---"
-mkdir -p "$SERVER_DIR" || { echo "Failed to create $INSTALL_DIR"; exit 1; }
+mkdir -p "$SERVER_DIR" || { echo "Failed to create $SERVER_DIR"; exit 1; }
 
 FILE=$(find "$TOOLS_DIR/build" -type f -name "spigot-*.jar")
 
@@ -29,7 +30,7 @@ else
   cp $FILE $SERVER_DIR/spigot.jar
 fi
 
-cp "./start_server.sh" "$SERVER_DIR/start_server.sh"
+cp "$SCRIPTS_DIR/start_server.sh" "$SERVER_DIR/start_server.sh"
 echo "eula=true" > "$SERVER_DIR/eula.txt"
 
 echo "--- Updating permissions 5/$SETUP_STEPS ---"
